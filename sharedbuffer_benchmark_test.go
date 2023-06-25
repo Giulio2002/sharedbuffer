897394c0ea7ecf89cb29cf1d577c54f02636c6a8
Giulio <giulio.rebuffo@gmail.com>
2023-06-25 17:53:48 +0200

    lol

--- sharedbuffer_benchmark_test.go
+++ sharedbuffer_benchmark_test.go
@@ -11,7 +11,7 @@ func BenchmarkContiguousLongLived(b *testing.B) {
 	maxRequest := 64
 	f := NewSimpleSharedBuffer(fsm.NewCountingFreeSpaceManager(), management.NewMemoryBuffer())
 	for i := 0; i < b.N; i++ {
-		f.Make(b.N % maxRequest)
+		f.Make(i % maxRequest)
 	}
 }
 
@@ -19,7 +19,7 @@ func BenchmarkBitmapLongLived(b *testing.B) {
 	maxRequest := 64
 	f := NewSimpleSharedBuffer(fsm.NewBitmapFreeSpaceManagerWithWordSize(64), management.NewMemoryBuffer())
 	for i := 0; i < b.N; i++ {
-		f.Make(b.N % maxRequest)
+		f.Make(i % maxRequest)
 	}
 }
 
@@ -27,56 +27,39 @@ func BenchmarkITreeLongLived(b *testing.B) {
 	maxRequest := 64
 	f := NewSimpleSharedBuffer(fsm.NewIntervalTreeFreeSpaceManager(), management.NewMemoryBuffer())
 	for i := 0; i < b.N; i++ {
-		f.Make(b.N % maxRequest)
+		f.Make(i % maxRequest)
 	}
 }
 
 func BenchmarkContiguousShortLived(b *testing.B) {
 	f := NewSimpleSharedBuffer(fsm.NewCountingFreeSpaceManager(), management.NewMemoryBuffer())
-	maxRequest := 64
-	batchSize := 64
-	for i := 0; i < b.N/batchSize; i++ {
-		cs := []func(){}
-		for j := 0; j < batchSize; j++ {
-			_, cf := f.Make(b.N % maxRequest)
-			cs = append(cs, cf)
-		}
-		for _, cf := range cs {
-			cf()
-		}
+	maxRequest := 100000
+	for i := 0; i < b.N; i++ {
+		_, cf := f.Make(maxRequest)
+		cf()
 	}
 }
 
 func BenchmarkBitmapShortLived(b *testing.B) {
-
 	f := NewSimpleSharedBuffer(fsm.NewBitmapFreeSpaceManagerWithWordSize(64), management.NewMemoryBuffer())
-
-	maxRequest := 64
-	batchSize := 64
-	for i := 0; i < b.N/batchSize; i++ {
-		cs := []func(){}
-		for j := 0; j < batchSize; j++ {
-			_, cf := f.Make(b.N % maxRequest)
-			cs = append(cs, cf)
-		}
-		for _, cf := range cs {
-			cf()
-		}
+	maxRequest := 100000
+	for i := 0; i < b.N; i++ {
+		_, cf := f.Make(maxRequest)
+		cf()
 	}
 }
 
 func BenchmarkITreeShortLived(b *testing.B) {
 	f := NewSimpleSharedBuffer(fsm.NewIntervalTreeFreeSpaceManager(), management.NewMemoryBuffer())
-	maxRequest := 64
-	batchSize := 64
-	for i := 0; i < b.N/batchSize; i++ {
-		cs := []func(){}
-		for j := 0; j < batchSize; j++ {
-			_, cf := f.Make(b.N % maxRequest)
-			cs = append(cs, cf)
-		}
-		for _, cf := range cs {
-			cf()
-		}
+	maxRequest := 100000
+	for i := 0; i < b.N; i++ {
+		_, cf := f.Make(maxRequest)
+		cf()
+	}
+}
+
+func BenchmarkAlloc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = make([]byte, 100000)
 	}
 }
